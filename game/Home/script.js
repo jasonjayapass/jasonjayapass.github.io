f42eacd7f03c43ff38f5caac95401b44c58a6679
jasonjayapass <Jayapass@gmail.com>
2022-02-01 19:17:47 -0700

    New Project

---
+++ game/Home/script.js
@@ -0,0 +1,11 @@
+function randomString() {
+	var chars = "ABCD";
+	var string_length = 1;
+	var randomstring = '';
+	for (var i=0; i<string_length; i++) {
+		var rnum = Math.floor(Math.random() * chars.length);
+		randomstring += chars.substring(rnum,rnum+1);
+	}
+	document.randform.randomfield.value = randomstring;
+}
+
